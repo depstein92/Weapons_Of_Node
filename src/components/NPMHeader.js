74ff94ba39e4951cddeb5811b7ea93a70a0794f6
Dan <d.epstein92@gmail.com>
2018-04-01 23:23:51 -0400

    added NPM Icon to SearchBar Component

--- src/components/NPMHeader.js
+++ src/components/NPMHeader.js
@@ -5,7 +5,6 @@ import { Image } from 'react-bootstrap';
 export default function NPMHeader(){
   return (
     <div className="npmIcon">
-     <img src="../styles/npm_icon" className='npmIcon'  />
     </div>
   )
 };
